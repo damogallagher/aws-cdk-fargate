e356e9ddeaf0cf35e97302ddb8681d7790f70d74
Damien Gallagher <damien.gallagher@gmail.com>
2024-01-31 15:18:47 +0000

    Update

--- app/app.js
+++ app/app.js
@@ -7,7 +7,6 @@ const products = [
     {id: 1, name: "Product 1", price: 100},
     {id: 2, name: "Product 2", price: 200},
     {id: 3, name: "Product 3", price: 300},
-    {port: PORT},
 ];
 
 app.get("/health", (req,res ) => {
